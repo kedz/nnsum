52c029301c27f68bc8d65f0ac05628f5d12902b1
kedz <kedzie@cs.columbia.edu>
2019-02-04 13:45:18 -0500

    Hyperparameter change.

--- fg_tests/test_pointer_generator_decoders/test_pointer_generator_decoder_forward.py
+++ fg_tests/test_pointer_generator_decoders/test_pointer_generator_decoder_forward.py
@@ -2,6 +2,18 @@ import pytest
 import torch
 
 
+def data_param_gen():
+    return [{"sparse": False}]
+
+
+def data_param_names(params):
+    return ("sp={sparse}").format(**params)
+
+@pytest.fixture(scope="module", params=data_param_gen(), 
+                ids=data_param_names)
+def data_params(request):
+    return request.param
+
 def decoder_param_gen():
     return [
         {"attention": "dot", "rnn_cell": "gru"},
@@ -31,7 +43,6 @@ def test_switch_probability(train_state, train_data):
     expected_gen = train_data["copy_probability"].t().masked_select(gen_mask)
     actual_gen = train_state["source_probability"].masked_select(gen_mask)
     assert torch.allclose(actual_gen, expected_gen, atol=1e-2)
-
 
     both_mask = train_data["copy_probability"].t().eq(.5)
     expected_both = train_data["copy_probability"].t().masked_select(both_mask)
